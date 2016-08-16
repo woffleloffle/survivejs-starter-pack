2d83a78831b7c251909743ea30f14a24f89a86a0
Derrynmitri <mitriderryn@gmail.com>
2016-08-17 01:06:24 +0200

    Display object from Jobs.json

--- app/components/Page.jsx
+++ app/components/Page.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-var mydata = "Test";
+var mydata = [{
+    "client": "Nam Interdum Enim Ltd",
+    "job-number": 1,
+    "job-name": "HB",
+    "due": "2014-07-20 05:34:16",
+    "status": "closed"
+  }];
 
   function display(){
-    return <div> {mydata}
+    return <div> {mydata[0].client}
   </div>;
   }
 
